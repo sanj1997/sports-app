5b62d7997522d6d16e60fd2b52a541920491ed6f
Js Sanjay <mname8741@gmail.com>
2022-12-21 01:06:23 +0530

    users-flow-line

--- backend/features/controllers/events.controller.js
+++ backend/features/controllers/events.controller.js
@@ -11,5 +11,36 @@ const createEvent=async(name,description,timing,place,capacity,userId)=>{
     }
     return response
 }
+const getAllEvents=async()=>{
+    let response;
+    try{
+         const allEvents=await EventsModel.find()
+         response={message:"Successful",data:allEvents}
+    }catch(e){
+        response={message:e.message}
+    }
+    return response
+}
 
-module.exports={createEvent}
+const getSingleEvent=async(id)=>{
+    let response;
+     try{
+          const singleEvent=await EventsModel.findOne({_id:id})
+          const details=singleEvent.timing.split("T")
+          const date=details[0]
+          const time=details[1]
+          const data={
+            name:singleEvent.name,
+            place:singleEvent.place,
+            date:date,
+            time:time,
+            capacity:singleEvent.capacity,
+            description:singleEvent.description
+          }
+          response={message:"Successful",data:data}
+     }catch(e){
+          response={message:e.message}
+     }
+    return response 
+}
+module.exports={createEvent,getAllEvents,getSingleEvent}
--- backend/features/routes/events.routes.js
+++ backend/features/routes/events.routes.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { createEvent } = require("../controllers/events.controller")
+const { createEvent, getAllEvents, getSingleEvent } = require("../controllers/events.controller")
 const authMiddleware = require("../middlewares/users.middleware")
 const route=express.Router()
 
@@ -11,5 +11,24 @@ route.post("/create",authMiddleware,async(req,res)=>{
         return res.send(response)
      }
     return res.status(401).send(response) 
+})
+
+route.get("/",async(req,res)=>{
+    const response=await getAllEvents()
+    if(response.message==="Successful")
+    {
+        return res.send(response)
+    }
+    return res.status(401).send(response)
+})
+
+route.get("/:id",authMiddleware,async(req,res)=>{
+    const {id}=req.params
+      const response=await getSingleEvent(id)
+      if(response.message==="Successful")
+      {
+        return res.send(response)
+      }
+    return res.status(401).send(response)  
 })
 module.exports=route
